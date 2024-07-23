548531cab70b7b5245f8a9d9285aed9c2bdb5334
sam599606 <sam599606@gmail.com>
2024-07-23 15:25:36 +0800

    First Commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { MdDelete, MdEdit } from "react-icons/md";
+import EditForm from "./Editform";
+
+function Todo({
+  todo,
+  deleteTodo,
+  toggleCompleted,
+  toggleIsEditing,
+  editTodo,
+}) {
+  return todo.isEditing ? (
+    <EditForm todo={todo} editTodo={editTodo} />
+  ) : (
+    <div className={`todo ${todo.isCompleted ? `completed` : ``}`}>
+      <p
+        onClick={() => {
+          toggleCompleted(todo.id);
+        }}
+      >
+        {todo.content}
+      </p>
+      <div>
+        <MdEdit
+          onClick={() => {
+            toggleIsEditing(todo.id);
+          }}
+          style={{ cursor: "pointer" }}
+        />
+        <MdDelete
+          onClick={() => {
+            deleteTodo(todo.id);
+          }}
+          style={{ cursor: "pointer", marginLeft: "5px" }}
+        />
+      </div>
+    </div>
+  );
+}
+export default Todo;
